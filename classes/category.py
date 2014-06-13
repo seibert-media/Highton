315bc22aced46d08d65ddbe200f3f358a73edde3
Michael Bykovski <mbykovski@seibert-media.net>
2014-06-13 17:58:59 +0200

    Fix documentation

--- classes/category.py
+++ classes/category.py
@@ -5,11 +5,6 @@ class Category(object):
         Feel free to use these attributes, they will be added if you call get_task_categories in the Highton-Class
         highrise_id,
         name,
-        updated_at,
-        account_id,
-        color,
-        created_at,
-        elements_count,
     """
     def save_data(self, category):
         self.highrise_id = category['id']
