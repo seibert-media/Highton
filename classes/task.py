4491471f11fc08f295352b0fb4711cbe1b77a8df
crank <crank@crank>
2014-06-20 00:31:53 +0200

    Updated Task Docu

--- classes/task.py
+++ classes/task.py
@@ -1,6 +1,33 @@
 
 
 class Task(object):
+    """
+        Feel free to use these attributes:
+        'recording-id',
+        'subject-id',
+        'subject-type',
+        'category-id',
+        'body',
+        'frame',
+        'due-at',
+        'alert-at',
+        'created-at',
+        'author-id',
+        'updated-at',
+        'public',
+        'recording-id',
+        'subject-id',
+        'subject-type',
+        'category-id',
+        'body',
+        'frame',
+        'due-at',
+        'alert-at',
+        'created-at',
+        'author-id',
+        'updated-at',
+        'public'
+    """
 
     def save_data(self, task):
         self.highrise_id = task['id']
