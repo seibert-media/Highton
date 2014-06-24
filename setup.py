82e05e4a8bb1a407014a5a7cc34bfb306c7efba5
Michael Bykovski <mbykovski@seibert-media.net>
2014-06-24 15:40:59 +0200

    Changed author email url keywords and version bump

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ packages = [
 
 setup(
     name = 'Highton',
-    version = '1.1',
+    version = '1.1.1',
     license = 'Apache License 2.0',
     description = 'A Python library for Highrise',
     long_description = 'A beautiful Python - Highrise - API. Less is more.',
-    url = 'https://github.com/bykof/Highton-API',
+    url = 'https://github.com/seibert-media/Highton',
     author = 'Michael Bykovski',
-    author_email = 'bykof@me.com',
+    author_email = 'mbykovski@seibert-media.net',
     py_modules=['highton', 'custom_exceptions'],
     packages=packages,
     install_requires = ['requests', 'lxml'],
-    keywords = 'bykof python api highrise highton',
+    keywords = 'bykof seibertmedia seibert media python api highrise highton',
     classifiers = [
         'Environment :: Web Environment',
         'Framework :: Django',
