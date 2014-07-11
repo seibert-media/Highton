20dabddefca1ba855886642a3a12093484caff96
Michael Bykovski <mbykovski@seibert-media.net>
2014-07-11 13:34:07 +0200

    Just wanted to make it safely

--- classes/tools.py
+++ classes/tools.py
@@ -6,12 +6,12 @@ def to_datetime(datetime_string):
     match = re.match('^(.*)-(.*)-(.*)( |T)(.*):(.*):(.*)(\+|Z).*$', datetime_string)
     if match:
         return datetime.datetime(
-            int(match.group(1)),
-            int(match.group(2)),
-            int(match.group(3)),
-            int(match.group(5)),
-            int(match.group(6)),
-            int(match.group(7)),
+            year=int(match.group(1)),
+            month=int(match.group(2)),
+            day=int(match.group(3)),
+            hour=int(match.group(5)),
+            minute=int(match.group(6)),
+            second=int(match.group(7)),
         )
     else:
         return None
