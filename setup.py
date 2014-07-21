6da3831762178afbbfc9c28617f317b3f10d033b
Michael Bykovski <mbykovski@seibert-media.net>
2014-07-21 11:49:34 +0200

    Version Bump to 1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ packages = [
 
 setup(
     name = 'Highton',
-    version = '1.1.5',
+    version = '1.2',
     license = 'Apache License 2.0',
     description = 'A Python library for Highrise',
     long_description = 'A beautiful Python - Highrise - API. Less is more.',
