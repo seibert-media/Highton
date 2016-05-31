29e5287cc72bafd225d45e9003955345d06f0b53
Michael Bykovski <mbykovski@seibert-media.net>
2016-05-31 23:48:01 +0200

    VERSION PUSH

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Highton',
-    version='1.2.5',
+    version='1.2.6',
     license='Apache License 2.0',
     description='A Python library for Highrise',
     long_description='A beautiful Python - Highrise - API. Less is more.',
