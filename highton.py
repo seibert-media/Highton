68ca7b1cc590546cdf8e53c20d96db2a860c96c4
loehnertz <jakob2102@gmail.com>
2017-09-13 16:00:52 +0200

    Implemented the remaining methods for POST, PUT and DELETE

--- highton.py
+++ highton.py
@@ -11,7 +11,7 @@ class Highton:
         self._api_key = api_key
 
     def _create_get_request(self, endpoint, params=None):
-        return requests.get(
+        get_response = requests.get(
             url='https://{user}.{highrise_url}/{endpoint}.xml'.format(
                 user=self._user,
                 highrise_url=Highton.HIGHRISE_URL,
@@ -21,17 +21,57 @@ class Highton:
                 username=self._api_key,
                 password=''
             ),
-            params=params
+            params=self._parse_from_dict_to_xml(params),
         )
 
+        return self._parse_from_xml_to_dict(get_response.content)
+
     def _create_post_request(self, endpoint, data=None):
-        pass
+        post_response = requests.post(
+            url='https://{user}.{highrise_url}/{endpoint}.xml'.format(
+                user=self._user,
+                highrise_url=Highton.HIGHRISE_URL,
+                endpoint=endpoint
+            ),
+            auth=HTTPBasicAuth(
+                username=self._api_key,
+                password=''
+            ),
+            data=self._parse_from_dict_to_xml(data),
+        )
+
+        return self._parse_from_xml_to_dict(post_response.content)
 
     def _create_put_request(self, endpoint, data=None):
-        pass
+        put_response = requests.put(
+            url='https://{user}.{highrise_url}/{endpoint}.xml'.format(
+                user=self._user,
+                highrise_url=Highton.HIGHRISE_URL,
+                endpoint=endpoint
+            ),
+            auth=HTTPBasicAuth(
+                username=self._api_key,
+                password=''
+            ),
+            data=self._parse_from_dict_to_xml(data),
+        )
+
+        return put_response.status_code
 
     def _create_delete_request(self, endpoint):
-        pass
+        delete_response = requests.delete(
+            url='https://{user}.{highrise_url}/{endpoint}.xml'.format(
+                user=self._user,
+                highrise_url=Highton.HIGHRISE_URL,
+                endpoint=endpoint
+            ),
+            auth=HTTPBasicAuth(
+                username=self._api_key,
+                password=''
+            ),
+        )
+
+        return delete_response.status_code
 
     def _parse_from_xml_to_dict(self, xml):
         pass
