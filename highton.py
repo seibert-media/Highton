1cda9134756ec617adee33357e275266d7561f55
loehnertz <jakob2102@gmail.com>
2017-09-14 14:11:21 +0200

    Refactored the method to make the actual requests to be a lot more generic

--- highton.py
+++ highton.py
@@ -1,12 +1,26 @@
+import logging
 import xmltodict
 import requests
 from requests.auth import HTTPBasicAuth
 
+logger = logging.getLogger(__name__)
+
 
 class Highton:
+    GET_REQUEST = 'GET'
+    POST_REQUEST = 'POST'
+    PUT_REQUEST = 'PUT'
+    DELETE_REQUEST = 'DELETE'
     HIGHRISE_URL = 'highrisehq.com'
 
+    class RequestException(Exception):
+        pass
+
     def __init__(self, user, api_key):
+        """
+        :param user: Your personal username for the Highrise API
+        :param api_key: Your personal API-Key for the Highrise API
+        """
         self._user = user
         self._api_key = api_key
 
@@ -18,75 +32,32 @@ class Highton:
     def _parse_from_dict_to_xml(dictionary):
         return xmltodict.unparse(dictionary)
 
-    def _create_get_request(self, endpoint, params=None):
-        get_response = requests.get(
-            url='https://{user}.{highrise_url}/{endpoint}.xml'.format(
-                user=self._user,
-                highrise_url=Highton.HIGHRISE_URL,
-                endpoint=endpoint
-            ),
-            auth=HTTPBasicAuth(
-                username=self._api_key,
-                password=''
-            ),
+    def _send_request(self, method, endpoint, params=None, data=None):
+        response = requests.request(
+            method=method,
+            url=f'https://{self._user}.{Highton.HIGHRISE_URL}/{endpoint}.xml',
+            headers={'Content-Type': 'application/xml'},
+            auth=HTTPBasicAuth(username=self._api_key, password=''),
             params=params if params else {},
+            data=data,
         )
 
-        return self._parse_from_xml_to_dict(get_response.content)
+        if not response.ok:
+            raise Highton.RequestException(response.status_code)
+        else:
+            return response
 
-    def _create_post_request(self, endpoint, data):
-        post_response = requests.post(
-            url='https://{user}.{highrise_url}/{endpoint}.xml'.format(
-                user=self._user,
-                highrise_url=Highton.HIGHRISE_URL,
-                endpoint=endpoint
-            ),
-            auth=HTTPBasicAuth(
-                username=self._api_key,
-                password=''
-            ),
-            headers={'Content-Type': 'application/xml'},
-            data=self._parse_from_dict_to_xml(data),
-        )
-
-        return self._parse_from_xml_to_dict(post_response.content)
-
-    def _create_put_request(self, endpoint, data):
-        put_response = requests.put(
-            url='https://{user}.{highrise_url}/{endpoint}.xml'.format(
-                user=self._user,
-                highrise_url=Highton.HIGHRISE_URL,
-                endpoint=endpoint
-            ),
-            auth=HTTPBasicAuth(
-                username=self._api_key,
-                password=''
-            ),
-            headers={'Content-Type': 'application/xml'},
-            data=self._parse_from_dict_to_xml(data),
-        )
-
-        return put_response.text
-
-    def _create_delete_request(self, endpoint):
-        delete_response = requests.delete(
-            url='https://{user}.{highrise_url}/{endpoint}.xml'.format(
-                user=self._user,
-                highrise_url=Highton.HIGHRISE_URL,
-                endpoint=endpoint
-            ),
-            auth=HTTPBasicAuth(
-                username=self._api_key,
-                password=''
-            ),
-        )
-
-        return delete_response.ok
-
-    def get_person(self, subject_id):
-        return self._create_get_request(
-            endpoint='people/{}'.format(subject_id),
-        )
-
-    def get_people(self):
-        pass
+    def _create_request(self, method, endpoint, params=None, data=None):
+        try:
+            return self._parse_from_xml_to_dict(
+                self._send_request(
+                    method=method,
+                    endpoint=endpoint,
+                    params=params,
+                    data=self._parse_from_dict_to_xml(data)
+                ).content
+            )
+        except Highton.RequestException as e:
+            logger.error(
+                f'The {method}-request towards the endpoint "/{endpoint}.xml" failed with HTTP-Code {str(e)}'
+            )
