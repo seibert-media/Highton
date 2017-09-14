a47d205f5b7795709b53e1f5f8372a633c3bf2f6
loehnertz <jakob2102@gmail.com>
2017-09-14 17:03:26 +0200

    Added first drafts of some API methods for the section 'People'

--- highton.py
+++ highton.py
@@ -16,6 +16,9 @@ class Highton:
     class RequestException(Exception):
         pass
 
+    class Fields:
+        pass
+
     def __init__(self, user, api_key):
         """
         :param user: Your personal username for the Highrise API
@@ -23,6 +26,10 @@ class Highton:
         """
         self._user = user
         self._api_key = api_key
+
+    """
+    General methods
+    """
 
     @staticmethod
     def _parse_from_xml_to_dict(xml):
@@ -38,7 +45,7 @@ class Highton:
             url=f'https://{self._user}.{Highton.HIGHRISE_URL}/{endpoint}.xml',
             headers={'Content-Type': 'application/xml'},
             auth=HTTPBasicAuth(username=self._api_key, password=''),
-            params=params if params else {},
+            params=params,
             data=data,
         )
 
@@ -47,17 +54,54 @@ class Highton:
         else:
             return response
 
-    def _create_request(self, method, endpoint, params=None, data=None):
+    def _make_request(self, method, endpoint, params=None, data=None):
         try:
-            return self._parse_from_xml_to_dict(
-                self._send_request(
-                    method=method,
-                    endpoint=endpoint,
-                    params=params,
-                    data=self._parse_from_dict_to_xml(data)
-                ).content
+            response = self._send_request(
+                method=method,
+                endpoint=endpoint,
+                params=params,
+                data=self._parse_from_dict_to_xml(data) if data else {}
             )
+
+            if len(response.content) > 1:
+                return self._parse_from_xml_to_dict(response.content)
+            else:
+                return response.status_code
         except Highton.RequestException as e:
             logger.error(
                 f'The {method}-request towards the endpoint "/{endpoint}.xml" failed with HTTP-Code {str(e)}'
             )
+
+    """
+    API methods
+    """
+
+    """
+    Section: People
+    Docs: https://github.com/basecamp/highrise-api/blob/master/sections/people.md
+    """
+    def get_person(self, subject_id):
+        return self._make_request(
+            method=Highton.GET_REQUEST,
+            endpoint=f'people/{subject_id}',
+        ).get('person')
+
+    def create_person(self, last_name, **kwargs):
+        return self._make_request(
+            method=Highton.POST_REQUEST,
+            endpoint='people',
+            data='',
+        )
+
+    def update_person(self, person):
+        return self._make_request(
+            method=Highton.PUT_REQUEST,
+            endpoint=f'people/{person["id"]["#text"]}',
+            data={'person': person},
+        )
+
+    def destroy_person(self, subject_id):
+        return self._make_request(
+            method=Highton.DELETE_REQUEST,
+            endpoint=f'people/{subject_id}',
+        )
