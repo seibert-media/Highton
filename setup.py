3e0b6efb71d89882420362851d2a9a098fab5acc
Michael Bykovski <bykof@me.com>
2017-10-04 17:10:13 +0200

    Latest Changes and RELEASE!

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ setup(
     version='2.0',
     license='Apache License 2.0',
     description='A Python library for Highrise',
-    long_description='A beautiful Python - Highrise - API. Less is more.',
+    long_description='A beautiful Python - Highrise - Wrapper.',
     url='https://github.com/seibert-media/Highton',
     author='Julia Giebelhausen, Jakob Löhnertz, Michael Bykovski',
     author_email='brogrammers@seibert-media.net',
-    packages=['highton', 'highton.classes'],
-    install_requires=[],
+    packages=['highton', 'highton.models', ],
+    install_requires=[
+        'requests'
+    ],
     keywords='seibertmedia seibert media python api wrapper highrise highton',
     classifiers=[
         'Environment :: Web Environment',
@@ -19,7 +21,13 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6.2',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
