f51fb3bb99a86d0d5018c1c3749d40679bc83c7d
Julian Felix Rost <jrost@seibert-media.net>
2018-01-11 15:35:05 +0100

    Increment version
    see #17

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Highton',
-    version='2.0.5',
+    version='2.0.6',
     license='Apache License 2.0',
     description='A Python library for Highrise',
     long_description='A beautiful Python - Highrise - Wrapper.',
